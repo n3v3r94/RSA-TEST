f4579c45bc24608d8421b534aba72678209c322c
Hristo <ivanovskihristo34@gmail.com>
2019-05-29 10:26:59 +0300

    Little fix

--- RSATestWithSmallNumber/Program.cs
+++ RSATestWithSmallNumber/Program.cs
@@ -9,112 +9,78 @@ namespace RSATestWithSmallNumber
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Random rnd = new Random();
-            BigInteger seed = new BigInteger(2432443243434343689);
-
-
-
-
-            BigInteger p1 = new BigInteger(885320963);
-            BigInteger p2 = new BigInteger(238855417);
-
-
-
-
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    p1 = rnd.Next(100000, int.MaxValue);
-            //    if (i > 10 && p1.IsEven)
-            //    {
-            //        break;
-            //    }
-            //}
-
-            //var temp = p1;
-            //for (int i = 0; i < 100000; i++)
-            //{
-            //    ++temp;
-            //    if (temp.IsEven)
-            //    {
-            //        p2 = temp;
-            //        break;
-            //    }
-
-            //}
-
-            Console.WriteLine($"p1= {p1.ToString().Length} {Environment.NewLine} p2= {p2.ToString().Length}");
-
-            BigInteger n = BigInteger.Multiply( p1 , p2);
-            BigInteger phiN = BigInteger.Multiply ((p1 - 1) , (p2 - 1));
-            Console.WriteLine($"Phi: {phiN.ToString().Length}");
-
-
-
-            int e = rnd.Next(1000, 10000);
-            BigInteger d;  /*116402471153538991; 2 * (phiN + 1) / e;*/
-
-            do
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            for (int k  = 0; k < 1; k++)
             {
+                Console.WriteLine("Please enter length of prime number 5 - 14");
+                int lenght = int.Parse(Console.ReadLine());
+
+                BigInteger p1 = GeneratePrimeNumber(lenght);
+                BigInteger p2 = GeneratePrimeNumber(lenght);
+
+                Console.WriteLine($"p1= {p1.ToString().Length} {Environment.NewLine} p2= {p2.ToString().Length}");
+
+                BigInteger n = BigInteger.Multiply(p1, p2);
+                BigInteger phiN = BigInteger.Multiply((p1 - 1), (p2 - 1));
+                Console.WriteLine($"Phi: {phiN} -> length {phiN.ToString().Length}");
+
+
+                BigInteger e = GeneratePublicExponentE(9, phiN);
+                Console.WriteLine($"e= {e}");
+
 
                 EuclidExtended ee = new EuclidExtended(e, phiN);
                 EuclidExtendedSolution result = ee.calculate();
 
-                if (result.X < 0)
+                BigInteger d = result.X < 0 ? BigInteger.Subtract(phiN, BigInteger.Abs(result.X)) : result.X;
+
+
+
+                Console.WriteLine($"d={d}");
+                if (e > n)
                 {
-                    d = phiN - BigInteger.Abs(result.X);
+                    Console.WriteLine("Sorry try again. n must must be biggest from e"); 
                 }
 
-                if (d * e % phiN == 1)
+                string messages = Console.ReadLine();
+
+                byte[] messagesInBytes = Encoding.UTF8.GetBytes(messages);
+
+                BigInteger[] cryptedMessages = new BigInteger[messages.Length];
+
+
+                foreach (var item in messagesInBytes)
                 {
-                    Console.WriteLine("success");
-                    break;
+                    if (item > n)
+                    {
+                        Console.WriteLine("Messages is broken. m must be smaller than n ");
+                        return;
+                    }
                 }
-
-                e = rnd.Next(1000, 10000);
-            } while (!(d * e % phiN == 1));
-
-
-
-
-            string messages = Console.ReadLine();
-
-            byte[] messagesInBytes = Encoding.UTF8.GetBytes(messages);
-
-            BigInteger[] cryptedMessages = new BigInteger[messages.Length];
-            Console.WriteLine($"d= {d}  e= {e}");
-
-            foreach (var item in messagesInBytes)
-            {
-                if (item > n)
+                Console.Write($"Crypted messages: ");
+                for (int i = 0; i < messagesInBytes.Length; i++)
                 {
-                    Console.WriteLine("Messages is broken. m must be smaller than n ");
-                    return;
-                }
-            }
-            Console.Write($"Crypted messages: ");
-            for (int i = 0; i < messagesInBytes.Length; i++)
-            {
-                cryptedMessages[i] = BigInteger.ModPow(messagesInBytes[i], e, n);
-               
+                    cryptedMessages[i] = BigInteger.ModPow(messagesInBytes[i], e, n);
+
                     Console.Write($"{(char)((cryptedMessages[i]) % char.MaxValue)} ");
+                }
+
+
+
+
+                BigInteger[] encryptedMessages = new BigInteger[messages.Length];
+                Console.WriteLine();
+                Console.Write($"Encrypted messages: ");
+                for (int i = 0; i < cryptedMessages.Length; i++)
+                {
+                    encryptedMessages[i] = BigInteger.ModPow(cryptedMessages[i], d, n);
+
+                    Console.Write($"{(char)(encryptedMessages[i])}");
+                }
+
+                Console.WriteLine();
             }
-
-
-
-
-            BigInteger[] encryptedMessages = new BigInteger[messages.Length];
-            Console.WriteLine();
-            Console.Write($"Encrypted messages: ");
-            for (int i = 0; i < cryptedMessages.Length; i++)
-            {
-                encryptedMessages[i] = BigInteger.ModPow(cryptedMessages[i], d, n);
-
-                Console.Write($"{(char)(encryptedMessages[i] )}");
-            }
-
-            Console.WriteLine();
-
 
             Console.ReadKey();
         }
@@ -128,44 +94,116 @@ namespace RSATestWithSmallNumber
                 Remainder = num1 % num2;
                 num1 = num2;
                 num2 = Remainder;
+
             }
-            Console.WriteLine(num1);
+
             return num1;
         }
 
-        void extended_euclid(int a, int b, int x1, int y1, int d)
+        public static BigInteger GeneratePrimeNumber(int lenght)
         {
-            int q, r, x2, y2, t;
-            x1 = 1; y1 = 0; x2 = 0; y2 = 1;
-            while (b != 0)
+            Random rnd = new Random();
+            BigInteger p = BigInteger.Parse(StartNumber(lenght));
+            string lenghtNumbers = StartNumberMax(lenght);
+            BigInteger test = BigInteger.Parse(lenghtNumbers);
+
+            for (int i = 2; i < p; i++)
             {
-                q = a / b;
-                r = a % b;
-                a = b;
-                b = r;
+               
+                if (test> int.MaxValue)
+                {
+                    p += rnd.Next(1, int.MaxValue);
+                }
 
-                t = x2; x2 = x1 - q * x2; x1 = t;
+                if (test<int.MaxValue)
+                {
+                    p += rnd.Next(1, int.Parse(StartNumberMax(lenght)));
+                }
 
-                t = y2; y2 = y1 - q * y2; y1 = t;
+                if (IsPrime(p))
+                {
+                    break;
+                }
             }
-            d = a;
+          
+
+            return p;
         }
 
-        //Hard code example
-        /*
-                    BigInteger p3 = 11;
-                    BigInteger p4 = 5;
-                    BigInteger n2 = p3 * p4;
-                    //BigInteger m = 30120;
-                    BigInteger e2 = 7;
-                    //BigInteger c2 = BigInteger.ModPow(m, e, n);
-                    BigInteger phiN2 = (p3 - 1) * (p4 - 1);
-                    EuclidExtended ee = new EuclidExtended(e, phiN);
-                    EuclidExtendedSolution result = ee.calculate();
-                    Console.WriteLine($"D={result.D} x={result.X} y={result.Y}");
-                    BigInteger d2 = (phiN2 - (-result.X)) ;
 
-                    //end example
-                    */
+        public static BigInteger GeneratePublicExponentE(int length, BigInteger PhiN)
+        {
+            BigInteger e = BigInteger.Parse(StartNumber(length));
+            Random rnd = new Random();
+            for (BigInteger i = e; i < PhiN; i++)
+            {
+                i += rnd.Next(1, 1000);
+                if (!(i.IsEven) && (GCD(i, PhiN) == 1))
+                {
+                    e = i;
+                    break;
+                }
+            }
+
+            return e;
+        }
+
+
+        public static string StartNumber(int lenght)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < lenght; i++)
+            {
+                if (i == 0)
+                {
+                    sb.Append("1");
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+        public static string StartNumberMax(int lenght)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < lenght; i++)
+            {
+                sb.Append("9");
+
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool IsPrime(BigInteger candidate)
+        {
+           
+            if ((candidate & 1) == 0)
+            {
+                if (candidate == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+           
+            for (BigInteger i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
+        }
+
     }
 }
